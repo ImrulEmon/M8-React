e1187e4d2d4a334d321a48f6c298a9e8f29fda73
ImrulEmon <emimrulemon@gmail.com>
2022-04-02 23:58:18 +0600

    :electron:Component

--- Module-44.5/react-core/src/App.js
+++ Module-44.5/react-core/src/App.js
@@ -4,22 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent brand='Apple' price='5000'></MyComponent>
+      <MyComponent brand='Google' price='1000'></MyComponent>
+      <MyComponent brand='Microsoft' price='1200'></MyComponent>
     </div>
   );
 }
-
+function MyComponent(props){
+  const myStyle={
+    backgroundColor: 'lightblue',
+    border:'3px solid blue',
+    margin:'20px',
+    padding:'10px',
+    borderRadius:'5px'
+  }
+  return(
+    <div style={myStyle}>
+      <h1>My own component = {props.brand}</h1>
+      <p style={{fontWeight:"bold",color:'orange'}}>Component written : <span style={{color:'black'}}>{props.price}</span></p>
+    </div>
+  )
+}
 export default App;
