c2ecea681ca303d884fc265e932022bdd1e5ad56
ImrulEmon <emimrulemon@gmail.com>
2022-04-04 17:42:37 +0600

    :electron:functional component

--- Module-47/understanding-react/src/App.js
+++ Module-47/understanding-react/src/App.js
@@ -1,28 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Device from './components/Device/Device';
 
 function App() {
   return (
     <div className="App">
-      <p>npm =  Node Package Manager</p>
-      <hr />
-      <p>cli = command line interface</p>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Device name="Phone" price='13500' />
     </div>
   );
 }
 
 export default App;
+
+{/* <p>npm =  Node Package Manager</p>
+<hr />
+<p>cli = command line interface</p>
+<hr />
+<p>Webpack,babel,settings</p>
+<header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  <p>
+    Edit <code>src/App.js</code> and save to reload.
+  </p>
+  <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+</header> */}
---
+++ Module-47/understanding-react/src/components/Config/Config.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Config = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>{props.price}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Config;
