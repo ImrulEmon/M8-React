8ca27ac05773c79887d24ff45c4cb2e192432b97
ImrulEmon <emimrulemon@gmail.com>
2022-04-05 14:46:39 +0600

    :electron: parent-clild

--- Module-47/understanding-react/src/App.js
+++ Module-47/understanding-react/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Device from './components/Device/Device';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [steps,setSteps] = useState(0);
+  const handleIncreaseSteps = () =>{
+    const newStepsCount = steps + 1;
+    setSteps(newStepsCount)
+  }
+
+  useEffect( ()=>{
+
+  }, [steps])
   return (
     <div className="App">
+      <h3>My Steps : {steps}</h3>
+      <button onClick={handleIncreaseSteps}>Walk</button>
      <Device name="Phone" price='13500' />
     </div>
   );
