d6f91a42212325ffb7fb99b225ac1fc611a43a60
ImrulEmon <emimrulemon@gmail.com>
2022-04-12 00:32:36 +0600

    :electron: localStorage

--- Module-48-P/ema-jhon-prac/src/components/Shop/Shop.js
+++ Module-48-P/ema-jhon-prac/src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -21,6 +22,7 @@ const Shop = () => {
     const handleAddToCart = (product) =>{
        const newCart = [...cart, product];
        setCart(newCart);
+       addToDb(product.key);
     }
 
     return (
