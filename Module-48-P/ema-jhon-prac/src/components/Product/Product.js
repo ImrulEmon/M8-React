032668a79e121b68557fb7cc3748e1199d1addfe
ImrulEmon <emimrulemon@gmail.com>
2022-04-11 15:04:37 +0600

    :electron: button icon

--- Module-48-P/ema-jhon-prac/src/components/Product/Product.js
+++ Module-48-P/ema-jhon-prac/src/components/Product/Product.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
+
+
 
 const Product = (props) => {
     const {name,img,price,seller,stock}=props.product;
+    const cartIcon = <FontAwesomeIcon icon={faShoppingCart}/>
     // console.log(props)
     return (
         <div className='product'>
@@ -14,7 +19,7 @@ const Product = (props) => {
                 <p> by : {seller}</p>
                 <p>Price : {price}</p>
                 <p><small>Only {stock} left in stock - order soon </small></p>
-                <button className='btn-regular' onClick={()=>props.handleAddToCart(props.product)}>add to cart</button>
+                <button className='btn-regular' onClick={()=>props.handleAddToCart(props.product)}>{cartIcon} add to cart</button>
             </div>
         </div>
     );
