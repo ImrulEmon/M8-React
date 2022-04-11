4595402b8637c7d24ba022102c377093e01594c8
ImrulEmon <emimrulemon@gmail.com>
2022-04-11 21:45:56 +0600

    :electron: ✔

--- Module-48-P/ema-jhon-prac/src/components/Cart/Cart.js
+++ Module-48-P/ema-jhon-prac/src/components/Cart/Cart.js
@@ -4,17 +4,26 @@ import './Cart.css'
 const Cart = (props) => {
 
     const {cart}=props;
-    let total = 0 ;
 
-    for (const product of cart){
-        total = total + product.price;
-    }
+    // let total = 0 ;
+    // for (const product of cart){
+    //     total = total + product.price;
+    // }
 
+    const totalReducer = (previous, product) => previous+product.price;
+
+    const total = cart.reduce(totalReducer,0);
+    const shipping = 15;
+    const tax = (total+shipping)/10;
+    const grandTotal = total + shipping + tax;
     return (
         <div>
              <h3>Order Summary</h3>
                 <h5>Items ordered : {props.cart.length}</h5>
                 <p>Total : {total.toFixed(2)}</p>
+                <p>Shipping : {shipping}</p>
+                <p>Tax : {tax.toFixed(2)}</p>
+                <h4 className='grandTotal'>Grand Total : {grandTotal.toFixed(2)}</h4>
         </div>
     );
 };
